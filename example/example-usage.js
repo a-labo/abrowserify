a1cd313700c2a3dd5aea1276a0d76f580611a58c
Taka Okunishi <okunishitaka.com@gmail.com>
2016-07-18 00:22:58 +0900

    Update deps

---
+++ example/example-usage.js
@@ -0,0 +1,3 @@
+'use strict'
+
+const abrowserify = require('abrowserify')
---
+++ test/abrowserify_test.js
@@ -0,0 +1,27 @@
+/**
+ * Test case for abrowserify.
+ * Runs with mocha.
+ */
+'use strict'
+
+const abrowserify = require('../lib/abrowserify.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('abrowserify', function () {
+  this.timeout(3000)
+
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Abrowserify', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
